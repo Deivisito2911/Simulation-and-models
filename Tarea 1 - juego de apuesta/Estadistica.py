8d925edd20d22bf1e445d140e151a4f736bc3f93
Deivith Zanella <correoserio29112002@gmail.com>
2024-10-11 18:27:17 -0400

    Documentacion del codigo

--- Tarea 1 - juego de apuesta/Estadistica.py
+++ Tarea 1 - juego de apuesta/Estadistica.py
@@ -7,26 +7,26 @@ def numeropar (num):
     else:
         return False
 
-def Lanzar () :
-    dado = random.randint(1,6)
-    monedaLanzada = random.choice(moneda)
+def Lanzar () : #Simulacion del lanzamiento
+    dado = random.randint(1,6)#Lanzamiento de dado
+    monedaLanzada = random.choice(moneda)#Lanzamiento de moneda
     esPar = numeropar(dado)
     if esPar == True and monedaLanzada == "cara":
-        return True
+        return True#Gana el jugador
     else:
-        return False
+        return False#Gana la casa
 
 def main() :
     contPerdida = 0
     contGanada = 0
     print("Bienvenido a la simulacion del lanzamiento")
-    n = int(input("Introduzca el numero de veces que se lanzara : "))
+    n = int(input("Introduzca el numero de veces que se lanzara : "))#Numero de lanzamientos
     for i in range(n):
         esGanador = Lanzar()
         if esGanador == False:
-            contPerdida+= 1
+            contPerdida+= 1#Gana la casa
         else:
-            contGanada+= 1
-    print("Ganadas : ",contGanada," Perdidas : ",contPerdida)
+            contGanada+= 1  #Gana el jugador
+    print("Ganadas : ",contGanada," Perdidas : ",contPerdida)#Estadisticas
 
 main()
--- Tarea 1 - juego de apuesta/juego.py
+++ Tarea 1 - juego de apuesta/juego.py
@@ -4,44 +4,44 @@ moneda = ["cara","sello"]
 repeticiones = 2
 Inicial = 1000
 
-def numeropar (num):
+def numeropar (num): #Funcion para identificar numero par
     if num % 2 == 0 :
         return True
     else:
         return False
 
-def Juego (monedas,apuesta) :
-    dado = random.randint(1,6)
-    monedaLanzada = random.choice(moneda)
+def Juego (monedas,apuesta) :#Funcion que simula el lanzamiento
+    dado = random.randint(1,6)#Dado de 1 a 6
+    monedaLanzada = random.choice(moneda) #cara o sello
     esPar = numeropar(dado)
     if esPar == True and monedaLanzada == "cara":
-        return (monedas + apuesta)
+        return (monedas + apuesta) #Gana el jugador
     else:
-        return (monedas - apuesta)
+        return (monedas - apuesta) #Gana la casa
 
 def main() :
-    meta = 2000
+    meta = 2000 #Meta para ganar
     contGanada = 0
     contPerdida = 0
     print("Bienvenido al juego de apuestas")
-    apuestaInicial = float(input("Introduzca su apuesta inicial : "))
-    for i in range(100):
+    apuestaInicial = float(input("Introduzca su apuesta inicial : "))#apuesta que se usara
+    for i in range(100): #Cantidad de juegos que se simularan
         cantMoneda = Inicial
         apuesta = apuestaInicial
         while cantMoneda < meta and cantMoneda > 0:
             inicial = cantMoneda
             cantMoneda = Juego(cantMoneda,apuesta)
-            if cantMoneda < inicial:
+            if cantMoneda < inicial:#Al ganar se dobla la apuesta
                 apuesta*=2
             else:
-                apuesta = apuestaInicial
+                apuesta = apuestaInicial#Al perder volvemos a apostar lo incial
             if apuesta > cantMoneda :
-                apuesta = cantMoneda
+                apuesta = cantMoneda#Si la apuesta supera la cantidad de monedas se apuesta todo
             #print("monedas : ",cantMoneda," apuesta : ",apuesta)
         if int(cantMoneda) == 0:
-            contPerdida+= 1
+            contPerdida+= 1#Gana la casa
         elif int(cantMoneda) >= meta:
-            contGanada+= 1
-    print("Ganadas : ",contGanada," Perdidas : ",contPerdida)
+            contGanada+= 1#Gana el jugador
+    print("Ganadas : ",contGanada," Perdidas : ",contPerdida)#Totalizacion
 
 main()
