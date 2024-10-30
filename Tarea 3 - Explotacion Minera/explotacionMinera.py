87f8e93c808a972346c8e1e64d3b3c7e478f44a2
Deivith Zanella <correoserio29112002@gmail.com>
2024-10-30 16:23:38 -0400

    documentation of code

--- Tarea 3 - Explotacion Minera/explotacionMinera.py
+++ Tarea 3 - Explotacion Minera/explotacionMinera.py
@@ -1,5 +1,5 @@
 import random
-probExito = 0.40
+probExito = 0.40#Creacion de varibales globales
 probFallar = 1 - probExito
 gananciaEmpresa = 0.60
 gananciaEstado = 1 - gananciaEmpresa
@@ -7,24 +7,23 @@ cantBarriles = 300000
 precioExploracion = 1000000
 precioBarril = 150
 cantExploraciones = 1000000
-cuentaEmpresa = 0
 
-def Probabilidad():
+def Probabilidad():#probabilidad de fallar o no una exploracion
     return random.choices([True, False], weights=[probExito, probFallar])[0]
 
-def exploracion(barriles,cuentaEmpresa,cuentaEstado):
+def exploracion(barriles,cuentaEmpresa,cuentaEstado):#Simulacion de distintas exploraciones
     exitos = 0
     for idx in range(cantExploraciones):
         cuentaEmpresa-= precioExploracion
-        if Probabilidad():
+        if Probabilidad():#Si aciertan se actualizan las variables
             exitos+=1
             barriles+= cantBarriles
             cuentaEmpresa+= ((cantBarriles*precioBarril)*gananciaEmpresa)
             cuentaEstado+= ((cantBarriles*precioBarril)*gananciaEstado)
     exito = exitos / cantExploraciones
-    return barriles,cuentaEmpresa,cuentaEstado,exito
+    return barriles,cuentaEmpresa,cuentaEstado,exito#Actualizacion de variables
 
-def main():
+def main():#simulacion
     barriles = 0
     cuentaEmpresa = 0
     cuentaEstado = 0
